d33f2c155bdf696dc8ca08d12f992f5566e6f11b
pallomagualter <pallomagualter@gmail.com>
2022-10-05 18:16:45 -0300

    folder organization of the project

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
