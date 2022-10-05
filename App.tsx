7bef40bc08273c6cc39a068c943c0b8555902c3f
pallomagualter <pallomagualter@gmail.com>
2022-10-05 17:01:06 -0300

    using property style

--- App.tsx
+++ App.tsx
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>I'm here!</Text>
+      <Text style={styles.title}>
+        Nome do evento
+      </Text>
+      <Text style={styles.text}>
+        Sexta, 5 de outubro de 2022.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: '700',
+    marginTop: 48
+  },
+  text: {
+    color: '#6B6B6B',
+    fontSize: 16,
   },
 });
